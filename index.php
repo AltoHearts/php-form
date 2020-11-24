57d6629f387f9bb0f32401de887a321a58177240
AltoHearts <72461104+AltoHearts@users.noreply.github.com>
2020-11-24 09:48:32 -0800

    adds radio buttons
    favorite colors, sundae toppings

--- index.php
+++ index.php
@@ -13,6 +13,15 @@ if(isset($_GET["FirstName"])){//show info
   <form action ="">
   <p>First Name: <input type="text" name="FirstName" /><br /></p>
   <p>Last Name: <input type="text" name="LastName" /><br /></p>
+  <p>Favorite Color:</p>
+  <p><input type="radio" name="FavoriteColor" value="red" /><br /> Red </p>
+  <p><input type="radio" name="FavoriteColor" value="blue" /><br /> Blue </p>
+  <p><input type="radio" name="FavoriteColor" value="yellow" /><br /> Yellow </p>
+  
+  <p>Sundae Toppings:</p>
+  <p><input type="radio" name="Toppings[]" value="oreos" /><br /> Oreos </p>
+  <p><input type="radio" name="Toppings[]" value="whipped cream" /><br /> Whipped Cream</p>
+  <p><input type="radio" name="Toppings[]" value="crushed nuts" /><br /> Crushed Nuts</p>
   <input type="submit" />
  </form> 
   ';
