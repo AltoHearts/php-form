64f46b15c8df32da390388f05f87d8735dbe4892
AltoHearts <72461104+AltoHearts@users.noreply.github.com>
2020-11-24 09:15:32 -0800

    vardump

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 //echo 'This is PHP!</p>';
 
 if(isset($_GET["FirstName"])){//show info
-  echo $_GET["FirstName"];
+ // echo $_GET["FirstName"];
+  
+  echo '<pre>';
+  var_dum($_GET);
+  echo '</pre>';
+  
 }else{//show form
   echo'
   <form action ="">
   First Name: <input type="text" name="FirstName" /><br />
   <input type="submit" />
-  
- 
  </form> 
   ';
 }
